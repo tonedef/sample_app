20c7af5ad8fdbbaa7d5d6d4b5996ae04617112f4
Alex Kobylk <alex@rb-d.com>
2012-11-15 02:12:41 -0800

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Alex Kobylk"
+    email                 "alex@rb-d.com"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
+end
+
